4ef72a31916a1e07635ecaed8ed17f670f59f83b
xieyupengzZ <1107761900@qq.com>
2018-04-13 21:08:34 +0800

    算法demo

---
+++ src/main/java/com/xieyupeng/springboot/Utils/SelectionSort.java
@@ -0,0 +1,41 @@
+package com.xieyupeng.springboot.Utils;
+
+/**
+ * Created by XYP on 2018/4/13.
+ * 选择排序
+ * 时间复杂度：O(n^2)
+ * 稳定的算法
+ * <a>http://www.cnblogs.com/skywang12345/p/3597641.html</a>
+ */
+public class SelectionSort {
+
+    static void sort(int[] array){
+
+        for(int i = 0 ; i < array.length-1; i ++){
+
+            int max = 0;
+            int maxIndex = 0;
+            int j = i;
+
+            for(;j < array.length ; j ++){
+                if(array[j]>max){
+                    max = array[j];
+                    maxIndex = j;
+                }
+            }
+
+            array[maxIndex] = array[i];
+            array[i] = max;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{3,2,5,4,8,7,9,10};
+        sort(array);
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+     }
+
+}
